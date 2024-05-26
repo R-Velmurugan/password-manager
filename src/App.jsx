9a7661adc8013635a91d12d545931977c189347b
vel <r.velmurugan1506@gmail.com>
2024-05-26 14:36:35 +0530

    added Router and Header component in generate password feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Sidebar from "./components/Sidebar/Sidebar";
+import {Route, Routes} from "react-router-dom";
+import PasswordGenerator from "./components/PasswordGenerator";
 
 function App() {
   return (
-    <main>
-      <Sidebar/>
-    </main>
+      <>
+          <div className="bg-gradient-to-b from-[#2e333d] to-[#22262d] flex ">
+              <Sidebar/>
+              <div className="flex-grow overflow-hidden" >
+                  <Routes>
+                      <Route path="/generate-password" element={<PasswordGenerator/>} />
+                  </Routes>
+              </div>
+          </div>
+
+      </>
   );
 }
 
